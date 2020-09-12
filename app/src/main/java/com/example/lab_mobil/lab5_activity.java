47211ae0e49a2ffc190049137a01cc8aab055918
Seyidzahra <zehra79294338846@gmail.com>
2020-09-12 14:52:34 +0700

    Добавила изменение

--- app/src/main/java/com/example/lab_mobil/lab5_activity.java
+++ app/src/main/java/com/example/lab_mobil/lab5_activity.java
@@ -9,7 +9,7 @@ import android.view.View;
 import android.widget.Toast;
 
 public class lab5_activity extends AppCompatActivity {
-
+    //
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
